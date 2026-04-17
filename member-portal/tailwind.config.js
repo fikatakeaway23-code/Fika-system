bb85bb55bfba1a88821fe9c1037ee02536dce48e
Aasish  Khanal <22f-bcs.19@texascollege.edu.np>
2026-04-17 16:13:29 +0545

    chore: scaffold member-portal with Vite + React + Tailwind

---
+++ member-portal/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary:   '#1D9E75',
+        secondary: '#2D6A4F',
+      },
+    },
+  },
+  plugins: [],
+};
